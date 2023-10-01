23a3f5edca6b794d7547c2e7d07119d47067de93
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2023-10-01 14:16:18 +0900

    chapter 10

--- src/main/java/com/example/springbootdeveloper/service/UserService.java
+++ src/main/java/com/example/springbootdeveloper/service/UserService.java
@@ -19,4 +19,9 @@ public class UserService {
                 .password(encoder.encode(request.getPassword()))
                 .build()).getId();
     }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected User"));
+    }
 }
